a5413f737d591eedc1e0d7b417483b0fdf1eb0c4
wachrwisw12 <wachrwisw12@gmail.com>
2026-03-05 14:51:02 +0700

    wwwwwsหห

--- single_instance_windows.go
+++ single_instance_windows.go
@@ -2,23 +2,22 @@
 
 package main
 
-import "golang.org/x/sys/windows"
+import (
+	"golang.org/x/sys/windows"
+)
 
 var mutex windows.Handle
 
 func alreadyRunning() bool {
-	name, _ := windows.UTF16PtrFromString("Global\\HDH_ROLLBACK_MUTEX")
-
-	m, err := windows.CreateMutex(nil, false, name)
+	name, _ := windows.UTF16PtrFromString("Global\\HDH_ROLLBACK_APP_V1_MUTEX")
+	m, err := windows.CreateMutex(nil, true, name)
 	if err != nil {
 		return false
 	}
 
 	mutex = m
 
-	lastErr := windows.GetLastError()
-
-	if lastErr == windows.ERROR_ALREADY_EXISTS {
+	if windows.GetLastError() == windows.ERROR_ALREADY_EXISTS {
 		return true
 	}
 
