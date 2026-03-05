7f080ef21321b933219abcaa3392d5936c3db76b
wachrwisw12 <wachrwisw12@gmail.com>
2026-03-05 14:57:41 +0700

    wwwwwำหำด

--- single_instance_windows.go
+++ single_instance_windows.go
@@ -3,23 +3,45 @@
 package main
 
 import (
+	"syscall"
+	"unsafe"
+
 	"golang.org/x/sys/windows"
 )
 
 var mutex windows.Handle
 
 func alreadyRunning() bool {
-	name, _ := windows.UTF16PtrFromString("Global\\HDH_ROLLBACK_APP_V1_MUTEX")
-	m, err := windows.CreateMutex(nil, true, name)
-	if err != nil {
-		return false
-	}
+	name, _ := windows.UTF16PtrFromString("Global\\HDH_ROLLBACK_MUTEX")
 
-	mutex = m
+	handle, err := windows.OpenMutex(windows.SYNCHRONIZE, false, name)
 
-	if windows.GetLastError() == windows.ERROR_ALREADY_EXISTS {
+	if err == nil {
+		windows.CloseHandle(handle)
+
+		focusExistingWindow()
+
 		return true
 	}
 
+	mutex, _ = windows.CreateMutex(nil, false, name)
+
 	return false
+}
+
+func focusExistingWindow() {
+	user32 := syscall.NewLazyDLL("user32.dll")
+
+	findWindow := user32.NewProc("FindWindowW")
+	setForeground := user32.NewProc("SetForegroundWindow")
+	showWindow := user32.NewProc("ShowWindow")
+
+	title, _ := windows.UTF16PtrFromString("hdh rollback")
+
+	hwnd, _, _ := findWindow.Call(0, uintptr(unsafe.Pointer(title)))
+
+	if hwnd != 0 {
+		showWindow.Call(hwnd, 9) // SW_RESTORE
+		setForeground.Call(hwnd)
+	}
 }
