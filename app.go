030db91db21445ba96021fe6bd755292f48a624e
wachrwisw12 <wachrwisw12@gmail.com>
2026-03-06 09:57:26 +0700

    rrrr

--- app.go
+++ app.go
@@ -350,7 +350,7 @@ type Release struct {
 }
 
 func (a *App) CheckUpdate(currentVersion string) (*UpdateInfo, error) {
-	url := "https://api.github.com/repos/skko-sakonakon/HDH-Rollback/releases/latest"
+	url := "https://api.github.com/repos/ict-ssj-sakon/HDH-Rollback/releases/latest"
 
 	resp, err := http.Get(url)
 	if err != nil {
